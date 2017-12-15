100ddf67fdb71b5bd8d31bf62b178b011499798b
jeorryb <jeorry@gmail.com>
2017-12-14 22:52:24 -0500

    add getting_data

---
+++ 08_Twitter_Sentiment_P1/config.py
@@ -0,0 +1,5 @@
+# Getting your API keys from Twitter - https://apps.twitter.com
+CONSUMER_KEY = 'QlMwY6PIrrG9rMNLPWaDXSUay'
+CONSUMER_SECRET = 'l6bALjf6i298SPc3d97JSpPnkyW8hzh3qrc9CWbrJ37APF2aXe'
+ACCESS_TOKEN = '394667554-jbK9Ar2ZB1exaiWTCsvfkxytso4jGxj6eZo5jbbU'
+ACCESS_SECRET = 'axC0rVa61qMFKu9lQ9NEwutQIIzrANMVLXdaaZ2F8tuXM'
